0c5edcc97bf70d7500c509e4348b807756691bbd
voltkrugers <80946089+voltkrugers@users.noreply.github.com>
2024-11-04 11:47:21 +0100

    Revert "Deplacement 4 direction"
    This reverts commit 4f57507831e2f8ad65a84a1d94525ad64b82a692.

--- Assets/_ChaosPath/Script/Controller.cs
+++ Assets/_ChaosPath/Script/Controller.cs
@@ -2,93 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Controller : MonoBehaviour
 {
-    public int playerId; 
-    public float moveSpeed = 5f;
-    private List<Command> commands = new List<Command>();
-    private bool isRecording = false;
-    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        if (isRecording)
-        {
-            RecordInput();
-        }
-    }
-
-    public void StartRecording()
-    {
-        isRecording = true;
-        commands.Clear();
-        StartCoroutine(StopRecordingAfterTime(10));
-    }
-
-    private void RecordInput()
-    {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            commands.Add(new Command("Right", Time.time));
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            commands.Add(new Command("Left", Time.time));
-        }        
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            commands.Add(new Command("Up", Time.time));
-        }       
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            commands.Add(new Command("Down", Time.time));
-        }
-    }
-
-    private IEnumerator StopRecordingAfterTime(float duration)
-    {
-        yield return new WaitForSeconds(duration);
-        isRecording = false;
-        Debug.Log($"Player {playerId} finished recording: {commands.Count} commands recorded.");
-    }
-
-    public void PlayCommands()
-    {
-        StartCoroutine(ExecuteCommands());
-    }
-
-    private IEnumerator ExecuteCommands()
-    {
-        for (int i = 0; i < commands.Count; i++)
-        {
-            Command currentCommand = commands[i];
-            float commandDuration = (i < commands.Count - 1) ? commands[i + 1].time - currentCommand.time : 0.1f;
-
-            float startTime = Time.time;
-            while (Time.time < startTime + commandDuration)
-            {
-                switch (currentCommand.direction)
-                {
-                    case "Right":
-                        Move(Vector2.right);
-                        break;
-                    case "Left":
-                        Move(Vector2.left);
-                        break;
-                    case "Up":
-                        Move(Vector2.up);
-                        break;
-                    case "Down":
-                        Move(Vector2.down);
-                        break;
-
-                }
-                yield return null;
-            }
-        }
-    }
-
-    private void Move(Vector2 direction)
-    {
-        transform.Translate(direction * moveSpeed * Time.deltaTime);
+        
     }
 }
