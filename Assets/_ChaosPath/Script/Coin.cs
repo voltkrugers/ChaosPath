8887b2413cf7603a013d64743b971eb66c895cc5
clementmonello <monelloclement@gmail.com>
2024-11-07 00:45:14 +0100

    Add coin powerup

---
+++ Assets/_ChaosPath/Script/Coin.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public GameObject target;
+    public float Speed=1f;
+    public float FollowingDistance = 1f;
+    public Animator anim;
+
+    void Start()
+    {
+        target = null;
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            float distance = (target.transform.position - gameObject.transform.position).magnitude;
+            if (distance > FollowingDistance)
+            {
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, target.transform.position, Speed * distance * Time.deltaTime);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            if (target != collision.gameObject)
+            {
+                anim.SetTrigger("Picked");
+            }
+            if (target != null)
+            {
+                //target.GetComponent<PlayerController>().HasCoin = false;
+            }
+
+            target = collision.gameObject;
+            //target.GetComponent<PlayerController>().HasCoin = true;
+            
+        }
+    }
+}
